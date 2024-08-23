e16902aad78e6c199dbac76054e4b465039956f4
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-23 18:15:26 -0400

    Replace placeholders across project

--- components/page_details_cards_component_1724451326.js
+++ components/page_details_cards_component_1724451326.js
@@ -5,16 +5,16 @@
     <h2 id="details-title" class="text-lg font-semibold mb-5 pl-4">Details</h2>
     <ul id="details-list" class="grid gap-4 min-[480px]:grid-cols-3 text-sm">
       <li id="project-length" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
-        <div class="font-medium text-slate-200">PAGE_DETAIL_1_KEY</div>
-        <div class="text-slate-400">PAGE_DETAIL_1_VALUE</div>
+        <div class="font-medium text-slate-200">Variety</div>
+        <div class="text-slate-400">Discover a wide range of toilet paper options, from ultra-soft to eco-friendly.</div>
       </li>
       <li id="start-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
-        <div class="font-medium text-slate-200" id="start-date-des-1">PAGE_DETAIL_2_KEY</div>
-        <time class="text-slate-400" id="start-time-des-1">PAGE_DETAIL_2_VALUE</time>
+        <div class="font-medium text-slate-200" id="start-date-des-1">Convenience</div>
+        <time class="text-slate-400" id="start-time-des-1">Order from the comfort of your home and enjoy fast, discreet delivery to your doorstep.</time>
       </li>
       <li id="end-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
-        <div class="font-medium text-slate-200" id="end-date-des-1">PAGE_DETAIL_3_KEY</div>
-        <time class="text-slate-400" id="end-time-des-1">PAGE_DETAIL_3_VALUE</time>
+        <div class="font-medium text-slate-200" id="end-date-des-1">Savings</div>
+        <time class="text-slate-400" id="end-time-des-1">Compare prices, find bulk deals, and save money on this essential household item.</time>
       </li>
     </ul>
   </section>
