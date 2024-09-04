60cc341a048504ca419f0d76c2473770983fa900
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-09-04 14:34:17 +0000

    Reset file
    File: components/purple_background_cta_component_1723693506.js
    Git Ref: 27fc284bb222f5cf61aafd593562d94210134f08
    Prune: False
    Extra Metadata: {"sourceRef":"c6541f11f1c0f451632c039d4643e7b61bb05be3","opType":"REDO"}

--- components/purple_background_cta_component_1723693506.js
+++ components/purple_background_cta_component_1723693506.js
@@ -26,6 +26,8 @@ Vue.component("purple_background_cta_component_1723693506", {
                     <div class="flex" id="content-title-container"><h2 id="content-title" class="flex-1 h2 bg-clip-text pb-4" :class="[transparentTextStyle, contentCtaTitleStyle]"> Roll into Savings: Your Toilet Paper Marketplace</h2></div>
                     <div class="flex" id="content-body-container"><p id="content-body" class="flex-1 text-lg mb-8" :class="[contentBodyStyle]">Discover the ultimate destination for all your bathroom tissue needs. From ultra-soft to eco-friendly options, we've got your bottom line covered.</p></div>
                     <a id="content-cta-button" href="#0" class="btn transition duration-150 ease-in-out group" :class="[contentCtaButtonPrimaryStyle, contentCtaButtonGradientStyle, contentCtaButtonGradientStyle, contentCtaButtonHoverStyle]">Get Started <span id="content-cta-arrow" class="tracking-normal group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1" :class="contentCtaArrowStyle">-&gt;</span></a>
+                    <!-- New Image Added -->
+                    <img id="cta-image" src="https://raw.githubusercontent.com/56b81caaa87941618cfed6dfb4d34047/Toilet_Paper_Marketplace_1723693503/main/images/ac5594df28494d1faa59f078f6edb4a0.jpeg" alt="Toilet Paper Marketplace" class="mt-8 mx-auto rounded" />
                 </div>
             </div>
         </div>
@@ -49,4 +51,3 @@ Vue.component("purple_background_cta_component_1723693506", {
             };
         },
     });
-                    
